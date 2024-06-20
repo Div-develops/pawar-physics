faa94b0c5a2619753b221c5ce50a42c529a6825d
Divya Pawar <divyapawar.sstc@gmail.com>
2024-06-20 19:50:27 +0530

    Update Profile.js

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -2,13 +2,13 @@ import React from 'react'
 import { useContext } from "react";
 import { Context as AuthContext } from "../context/AuthContext";
 import './profile.css'
-import Navbar from '../Navbar/Navbar';
+
 function Profile() {
     const { user } = useContext(AuthContext);
     const displayName = user.email.split("@")[0]
     return (
-        <div style={{ backgroundColor: "#444444", height: "100vh", display: "flex",flexDirection: "column", alignItems: "center",gap:"30px"}}>
-            <div style={{marginTop:"50px",fontSize:"40px",fontFamily:"Poppins",fontWeight:"400",color:"#ffff"}}>Profile Page</div>
+        <div className='profile-container'>
+            <div className='profile-header'>Your Profile</div>
 
             <div className="profile-box">
 
@@ -21,7 +21,7 @@ function Profile() {
                 </div>
                 <div className="profile-details">
                     <div className="profile-username">Username: {displayName}</div>
-                    <div className="profile-email">Email: {user.email}</div>
+                    <div className="profile-email" style={{marginTop:"10px"}}>Email: {user.email}</div>
                 </div>
             </div>
         </div>
